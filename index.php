61dc23d0c1b3f10d34d42c2c11921b5cfb7b9a54
faynald <faynald@gmail.com>
2021-05-16 22:37:41 +0700

    add sidebar and welcome page

--- index.php
+++ index.php
@@ -9,9 +9,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 </head>
 <body>
-    
+    <!-- Sidebar -->
+    <div class="sidenav">
+        <div class="sidenav-header horizontal-center">
+            <i class="fa fa-bars pr-10"></i> Menu
+        </div>
+        <a href="transaksi.php"><i class="fa fa-book pr-15"></i> Transaksi</a>
+        <a href="laporan.php"><i class="fa fa-pencil pr-15"></i> Laporan</a>
+    </div>
+    <!-- Konten -->
+    <div class="main center">
+            <div class="row">
+                <i class="fa fa-user-circle fa-10x horizontal-center" aria-hidden="true"></i>
+            </div>
+            <div class="row horizontal-center">
+                <h1>
+                    Selamat Datang, $user
+                </h1>
+            </div>
+            <div class="row horizontal-center">
+                <a href="login.php"><button class="logout-button">Logout</button></a>
+            </div>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
 <script src="js/javascript.js"></script>
+<!-- fontawesome -->
+<script src="https://kit.fontawesome.com/3e7c653e94.js" crossorigin="anonymous"></script>
 </html>
