a51e08c48c7e0a660fe0e497c1204e40e6db67b4
faynald <faynald@gmail.com>
2021-05-20 19:54:27 +0700

    add tambah buku page

---
+++ page/tambah-buku.php
@@ -0,0 +1,31 @@
+<div class="main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-10 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Tambah Buku</h1>
+                    </div>
+                    <div class="card-body">
+                        <label>ID Buku</label>
+                        <input class="form-control" type="text" placeholder="AUTO INCREMENT" aria-label="Disabled input example" disabled>
+                        <label>Judul Buku</label>
+                        <input class="form-control" type="text" placeholder="Sampang adalah kampung halamanku">
+                        <label>Pengarang</label>
+                        <input class="form-control" type="text" placeholder="Rijal Racing">
+                        <label>Penerbit</label>
+                        <input class="form-control" type="text" placeholder="Rijal Studio">
+                        <label>Harga</label>
+                        <input class="form-control" type="text" placeholder="Rp 900.000">
+                        <label>Stock</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
