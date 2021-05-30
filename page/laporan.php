9aeb505ccf9333cd1aceb863683911f8d4d208be
faynald <faynald@gmail.com>
2021-05-31 01:27:28 +0700

    add image overlay in laporan.php

--- page/laporan.php
+++ page/laporan.php
@@ -18,18 +18,21 @@
                 <div class="card card-stats">
                     <!-- Card body -->
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Total Buku Terjual :</h5>
-                                <span class="h2 font-weight-bold mb-0">
-                                    <label id="periodBalance">
-                                        <?= $count_terjual ?>
-                                    </label>
-                                </span>
-                            </div>
-                            <div class="col-auto">
-                                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                                    <i class="ni ni-chart-bar-32"></i>
+                        <img src="./images/book sold.png" class="card-img" alt="" style="width: 9rem;">
+                        <div class="card-img-overlay">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Total Buku Terjual </h5>
+                                    <span class="h2 font-weight-bold mb-0">
+                                        <label id="periodBalance">
+                                            <?= $count_terjual ?>
+                                        </label>
+                                    </span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                                        <i class="ni ni-chart-bar-32"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -40,14 +43,17 @@
                 <div class="card card-stats">
                     <!-- Card body -->
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Stok buku tersedia</h5>
-                                <span class="h2 font-weight-bold mb-0"><label id="balance"><?= $count_stoktersedia ?></label></span>
-                            </div>
-                            <div class="col-auto">
-                                <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
-                                    <i class="ni ni-chart-pie-35"></i>
+                        <img src="./images/book.png" class="card-img" alt="" style="width: 9rem;">
+                        <div class="card-img-overlay">
+                            <div class="row">
+                                <div class="col">
+                                    <h5 class="card-title text-uppercase text-muted mb-0">Stok buku tersedia</h5>
+                                    <span class="h2 font-weight-bold mb-0"><label id="balance"><?= $count_stoktersedia ?></label></span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                                        <i class="ni ni-chart-pie-35"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
